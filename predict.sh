dd34a38e95b43f956998651515da8c778018a7af
Josh Bailey <josh@vandervecken.com>
2024-08-17 03:01:41 +0000

    predict/train

--- predict.sh
+++ predict.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 set -o noglob
-exec docker run --gpus=all --rm -v /scratch:/scratch -ti sidtableflip /sidtableflip/predict.py $*
+exec docker run --gpus=all --rm -v /scratch:/scratch -ti anarkiwi/sidtableflip /sidtableflip/predict.py $*
--- train.sh
+++ train.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 set -o noglob
-exec docker run --gpus=all --rm -v /scratch:/scratch -ti sidtableflip /sidtableflip/train.py $*
+exec docker run --gpus=all --rm -v /scratch:/scratch -ti anarkiwi/sidtableflip /sidtableflip/train.py $*
