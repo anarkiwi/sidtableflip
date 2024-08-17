7d928ada170d84ea724aafdcfa42466714f01639
Josh Bailey <josh@vandervecken.com>
2024-08-17 05:17:09 +0000

    port fairq

--- sidtableflip/args.py
+++ sidtableflip/args.py
@@ -2,23 +2,25 @@ def add_args(parser):
     parser.add_argument(
         "--reglogs",
         type=str,
-        default="/scratch/hvsc/C64Music/MUSICIANS/H/Hubbard_Rob/*/*/*-1.dump.zst",
-        # default="/scratch/hvsc/C64Music/MUSICIANS/J/Jammer/Grid_Runner/1/Grid_Runner-1.dump.zst",
+        # default="/scratch/tmp/goto80/*zst",
+        # default="/scratch/hvsc/C64Music/MUSICIANS/G/Goto80/*/*/*-1.dump.zst",
+        # default="/scratch/hvsc/C64Music/MUSICIANS/H/Hubbard_Rob/*/*/*-1.dump.zst",
+        default="/scratch/hvsc/C64Music/MUSICIANS/J/Jammer/Grid_Runner/1/Grid_Runner-1.dump.zst",
     )
     parser.add_argument("--model_state", type=str, default="/scratch/tmp/model.pth")
-    parser.add_argument("--sequence-length", type=int, default=1024)
-    parser.add_argument("--max-epochs", type=int, default=5)
-    parser.add_argument("--batch-size", type=int, default=12)
+    parser.add_argument("--sequence-length", type=int, default=2048)
+    parser.add_argument("--max-epochs", type=int, default=3)
+    parser.add_argument("--batch-size", type=int, default=4)
     parser.add_argument("--wav", type=str, default="/scratch/tmp/sidtableflip.wav")
     parser.add_argument("--csv", type=str, default="/scratch/tmp/sidtableflip.csv")
     parser.add_argument("--output-cycles", type=int, default=60e6)
     parser.add_argument("--shuffle", type=bool, default=True)
-    parser.add_argument("--max-files", type=int, default=32)
+    parser.add_argument("--max-files", type=int, default=64)
     parser.add_argument("--diffq", type=int, default=4)
-    parser.add_argument("--learning-rate", type=float, default=1e-5)
+    parser.add_argument("--learning-rate", type=float, default=1e-4)
     parser.add_argument("--token-csv", type=str, default=None)
-    parser.add_argument("--layers", type=int, default=8)
+    parser.add_argument("--layers", type=int, default=4)
     parser.add_argument("--heads", type=int, default=4)
-    parser.add_argument("--embed", type=int, default=512)
+    parser.add_argument("--embed", type=int, default=256)
     parser.add_argument("--optimizer", type=str, default="adam")
     return parser
--- sidtableflip/model.py
+++ sidtableflip/model.py
@@ -3,52 +3,48 @@ import torch
 from torch import nn
 import torch.nn.functional as F
 
+# from fairseq.modules.positional_encoding import PositionalEncoding
 # https://github.com/pytorch/examples/blob/main/word_language_model/model.py
 
 
-# Temporarily leave PositionalEncoding module here. Will be moved somewhere else.
+def model_div_term(d_model):
+    return torch.exp(
+        torch.arange(0, d_model, 2, dtype=torch.float32)
+        * -(math.log(10000.0) / d_model)
+    )
+
+
 class PositionalEncoding(nn.Module):
-    r"""Inject some information about the relative or absolute position of the tokens in the sequence.
-        The positional encodings have the same dimension as the embeddings, so that the two can be summed.
-        Here, we use sine and cosine functions of different frequencies.
-    .. math:
-        \text{PosEncoder}(pos, 2i) = sin(pos/10000^(2i/d_model))
-        \text{PosEncoder}(pos, 2i+1) = cos(pos/10000^(2i/d_model))
-        \text{where pos is the word position and i is the embed idx)
+    """Positional encoding.
+
     Args:
-        d_model: the embed dim (required).
-        dropout: the dropout value (default=0.1).
-        max_len: the max. length of the incoming sequence (default=5000).
-    Examples:
-        >>> pos_encoder = PositionalEncoding(d_model)
+        d_model: Embedding dimension.
+        dropout_rate: Dropout rate.
+        max_len: Maximum input length.
     """
 
-    def __init__(self, d_model, max_len, dropout):
-        super(PositionalEncoding, self).__init__()
-        self.dropout = nn.Dropout(p=dropout)
-
-        pe = torch.zeros(max_len, d_model)
-        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
-        div_term = torch.exp(
-            torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model)
-        )
+    def __init__(self, d_model, max_len, device, dropout_rate):
+        """Construct an PositionalEncoding object."""
+        super().__init__()
+        self.d_model = d_model
+        self.xscale = math.sqrt(self.d_model)
+        self.dropout = nn.Dropout(p=dropout_rate)
+        pe = torch.zeros(max_len, self.d_model)
+        position = torch.arange(0, max_len, dtype=torch.float32).unsqueeze(1)
+        div_term = model_div_term(self.d_model)
         pe[:, 0::2] = torch.sin(position * div_term)
         pe[:, 1::2] = torch.cos(position * div_term)
-        pe = pe.unsqueeze(0).transpose(0, 1)
-        self.register_buffer("pe", pe)
+        pe = pe.unsqueeze(0)
+        self.pe = pe.to(device=device, dtype=torch.float32)
 
-    def forward(self, x):
-        r"""Inputs of forward function
+    def forward(self, x: torch.Tensor):
+        """Add positional encoding.
         Args:
-            x: the sequence fed to the positional encoder model (required).
-        Shape:
-            x: [sequence length, batch size, embed dim]
-            output: [sequence length, batch size, embed dim]
-        Examples:
-            >>> output = pos_encoder(x)
+            x (torch.Tensor): Input tensor B X T X C
+        Returns:
+            torch.Tensor: Encoded tensor B X T X C
         """
-
-        x = x + self.pe[: x.size(0), :]
+        x = x * self.xscale + self.pe[:, : x.size(1)]
         return self.dropout(x)
 
 
@@ -73,12 +69,13 @@ class TransformerModel(nn.Transformer):
             device=device,
             batch_first=True,
         )
-        self.pos_encoder = PositionalEncoding(embed_dim, dataset.n_vocab, dropout)
+        self.pos_encoder = PositionalEncoding(
+            embed_dim, sequence_length, device, dropout
+        )
         self.input_emb = nn.Embedding(dataset.n_vocab, embed_dim)
         self.src_mask = self.generate_square_subsequent_mask(
             sequence_length, device=device
         )
-        self.ninp_sqrt = math.sqrt(embed_dim)
         self.init_weights()
 
     def init_weights(self, initrange=0.1):
@@ -87,7 +84,7 @@ class TransformerModel(nn.Transformer):
         nn.init.uniform_(self.decoder.weight, -initrange, initrange)
 
     def forward(self, src):
-        src = self.input_emb(src) * self.ninp_sqrt
+        src = self.input_emb(src)
         src = self.pos_encoder(src)
         output = self.encoder(src, mask=self.src_mask, is_causal=True)
         output = self.decoder(output)
