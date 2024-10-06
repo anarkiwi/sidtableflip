15004404affc7d91113aed72214da97c4bdb9add
Josh Bailey <josh@vandervecken.com>
2024-10-06 02:42:25 +0000

    no opt

--- sidtableflip/args.py
+++ sidtableflip/args.py
@@ -31,6 +31,5 @@ def add_args(parser):
     parser.add_argument("--norm_eps", type=float, default=1e-5)
     parser.add_argument("--rope_base", type=int, default=10000)
     parser.add_argument("--attn_dropout", type=float, default=0)
-    parser.add_argument("--optimizer", type=str, default="adam")
     parser.add_argument("--model", choices=list(MODEL_GETTERS.keys()), default="llama2")
     return parser
