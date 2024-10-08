5d0da8614df3ce3876e288dcbcab8e4eb7574320
Josh Bailey <josh@vandervecken.com>
2024-10-08 19:33:22 +0000

    paths.

--- sidtableflip/args.py
+++ sidtableflip/args.py
@@ -5,20 +5,20 @@ def add_args(parser):
     parser.add_argument(
         "--reglogs",
         type=str,
-        default="/scratch/tmp/goto80/*zst",
-        # default="/scratch/hvsc/C64Music/MUSICIANS/G/Goto80/*/*/*-1.dump.zst",
+        # default="/scratch/tmp/goto80/*zst",
+        default="/scratch/hvsc/C64Music/MUSICIANS/G/Goto80/*/*/*-1.dump.zst",
         # default="/scratch/hvsc/C64Music/MUSICIANS/H/Hubbard_Rob/*/*/*-1.dump.zst",
         # default="/scratch/hvsc/C64Music/MUSICIANS/J/Jammer/Grid_Runner/1/Grid_Runner-1.dump.zst",
     )
     parser.add_argument(
-        "--model_state", type=str, default="/scratch/tmp/sidtableflip.pth"
+        "--model_state", type=str, default="/scratch/sidtableflip/sidtableflip.pth"
     )
     parser.add_argument("--sequence-length", type=int, default=2048)
     parser.add_argument("--max-sequence-length", type=int, default=2048 * 10)
     parser.add_argument("--max-epochs", type=int, default=4)
     parser.add_argument("--batch-size", type=int, default=4)
-    parser.add_argument("--wav", type=str, default="/scratch/tmp/sidtableflip.wav")
-    parser.add_argument("--csv", type=str, default="/scratch/tmp/sidtableflip.csv")
+    parser.add_argument("--wav", type=str, default="/scratch/sidtableflip/sidtableflip.wav")
+    parser.add_argument("--csv", type=str, default="/scratch/sidtableflip/sidtableflip.csv")
     parser.add_argument("--output-cycles", type=int, default=60e6)
     parser.add_argument("--shuffle", type=bool, default=True)
     parser.add_argument("--max-files", type=int, default=128)
