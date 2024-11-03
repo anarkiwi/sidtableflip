e967704341a4db677040327a9dd32099775c3c62
Josh Bailey <josh@vandervecken.com>
2024-11-03 20:32:48 +1300

    2048

--- sidtableflip/args.py
+++ sidtableflip/args.py
@@ -20,8 +20,8 @@ def add_args(parser):
         "--model_state", type=str, default="/scratch/sidtableflip/sidtableflip.pth"
     )
     parser.add_argument("--tb_logs", type=str, default="/scratch/sidtableflip/tb_logs")
-    parser.add_argument("--sequence-length", type=int, default=4096)
-    parser.add_argument("--max-sequence-length", type=int, default=4096)
+    parser.add_argument("--sequence-length", type=int, default=2048)
+    parser.add_argument("--max-sequence-length", type=int, default=2048)
     parser.add_argument("--max-epochs", type=int, default=20)
     parser.add_argument("--batch-size", type=int, default=8)
     parser.add_argument(
@@ -39,7 +39,7 @@ def add_args(parser):
     parser.add_argument(
         "--token-csv", type=str, default="/scratch/sidtableflip/tokens.csv"
     )
-    parser.add_argument("--layers", type=int, default=2)
+    parser.add_argument("--layers", type=int, default=4)
     parser.add_argument("--heads", type=int, default=2)
     parser.add_argument("--kv_heads", type=int, default=None)
     parser.add_argument("--embed", type=int, default=128)
