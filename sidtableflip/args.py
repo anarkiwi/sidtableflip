6ae3028f9d8ca22650c0b9f9ec409359f1fe3e4e
Josh Bailey <josh@vandervecken.com>
2024-11-13 01:42:40 +0000

    32

--- sidtableflip/args.py
+++ sidtableflip/args.py
@@ -23,7 +23,7 @@ def add_args(parser):
     parser.add_argument("--sequence-length", type=int, default=8192)
     parser.add_argument("--max-sequence-length", type=int, default=8192)
     parser.add_argument("--max-epochs", type=int, default=20)
-    parser.add_argument("--batch-size", type=int, default=16)
+    parser.add_argument("--batch-size", type=int, default=32)
     parser.add_argument(
         "--wav", type=str, default="/scratch/sidtableflip/sidtableflip.wav"
     )
