f4ddc6ddd5ecb2d2ebe4803b12c95e087a72512a
Josh Bailey <josh@vandervecken.com>
2024-11-16 02:14:12 +0000

    args.

--- sidtableflip/args.py
+++ sidtableflip/args.py
@@ -6,9 +6,9 @@ def add_args(parser):
     parser.add_argument(
         "--reglogs",
         type=str,
-        # default="/scratch/hvsc/C64Music/MUSICIANS/G/Goto80/*/*/*-1.dump.zst",
+        default="/scratch/hvsc/C64Music/MUSICIANS/G/Goto80/*/*/*-1.dump.zst",
         # default="/scratch/hvsc/C64Music/MUSICIANS/H/Hubbard_Rob/*/*/*-1.dump.zst",
-        default="/scratch/hvsc/C64Music/MUSICIANS/J/Jammer/Grid_Runner/1/Grid_Runner-1.dump.zst",
+        # default="/scratch/hvsc/C64Music/MUSICIANS/J/Jammer/Grid_Runner/1/Grid_Runner-1.dump.zst",
     )
     parser.add_argument(
         "--reglog",
@@ -16,7 +16,7 @@ def add_args(parser):
         default="",
     )
     parser.add_argument(
-        "--model_state", type=str, default="/scratch/sidtableflip/sidtableflip.pth"
+        "--model_state", type=str, default="/scratch/sidtableflip/sidtableflip.ckpt"
     )
     parser.add_argument("--tb_logs", type=str, default="/scratch/sidtableflip/tb_logs")
     parser.add_argument("--sequence-length", type=int, default=2048)
@@ -38,8 +38,8 @@ def add_args(parser):
     parser.add_argument(
         "--token-csv", type=str, default="/scratch/sidtableflip/tokens.csv"
     )
-    parser.add_argument("--layers", type=int, default=2)
-    parser.add_argument("--heads", type=int, default=2)
+    parser.add_argument("--layers", type=int, default=4)
+    parser.add_argument("--heads", type=int, default=4)
     parser.add_argument("--kv_heads", type=int, default=None)
     parser.add_argument("--embed", type=int, default=128)
     parser.add_argument("--intermediate", type=int, default=None)
