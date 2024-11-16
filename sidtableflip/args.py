0c6573255319dbec6348bec6d225228eb837876c
Josh Bailey <josh@vandervecken.com>
2024-11-16 20:24:04 +0000

    argz.

--- sidtableflip/args.py
+++ sidtableflip/args.py
@@ -9,7 +9,7 @@ def add_args(parser):
         default="/scratch/hvsc/C64Music/MUSICIANS/G/Goto80/*/*/*-1.dump.zst",
         # default="/scratch/hvsc/C64Music/MUSICIANS/H/Hubbard_Rob/*/*/*-1.dump.zst",
         # default="/scratch/hvsc/C64Music/MUSICIANS/J/Jammer/Grid_Runner/1/Grid_Runner-1.dump.zst",
-    )
+    ) 
     parser.add_argument(
         "--reglog",
         type=str,
@@ -19,10 +19,10 @@ def add_args(parser):
         "--model_state", type=str, default="/scratch/sidtableflip/sidtableflip.ckpt"
     )
     parser.add_argument("--tb_logs", type=str, default="/scratch/sidtableflip/tb_logs")
-    parser.add_argument("--sequence-length", type=int, default=2048)
-    parser.add_argument("--max-sequence-length", type=int, default=2048)
+    parser.add_argument("--sequence-length", type=int, default=4096)
+    parser.add_argument("--max-sequence-length", type=int, default=4096*10)
     parser.add_argument("--max-epochs", type=int, default=20)
-    parser.add_argument("--batch-size", type=int, default=32)
+    parser.add_argument("--batch-size", type=int, default=16)
     parser.add_argument(
         "--wav", type=str, default="/scratch/sidtableflip/sidtableflip.wav"
     )
@@ -38,16 +38,16 @@ def add_args(parser):
     parser.add_argument(
         "--token-csv", type=str, default="/scratch/sidtableflip/tokens.csv"
     )
-    parser.add_argument("--layers", type=int, default=4)
-    parser.add_argument("--heads", type=int, default=4)
+    parser.add_argument("--layers", type=int, default=8)
+    parser.add_argument("--heads", type=int, default=8)
     parser.add_argument("--kv_heads", type=int, default=None)
-    parser.add_argument("--embed", type=int, default=256)
+    parser.add_argument("--embed", type=int, default=512)
     parser.add_argument("--intermediate", type=int, default=None)
     parser.add_argument("--norm_eps", type=float, default=1e-5)
     parser.add_argument("--rope_base", type=int, default=10000)
     parser.add_argument("--start_n", type=int, default=None)
     parser.add_argument("--attn_dropout", type=float, default=0)
-    parser.add_argument("--model", choices=list(MODEL_GETTERS.keys()), default="phi3")
+    parser.add_argument("--model", choices=list(MODEL_GETTERS.keys()), default="llama2")
     parser.add_argument(
         "--precision", choices=["highest", "high", "medium"], default="medium"
     )
