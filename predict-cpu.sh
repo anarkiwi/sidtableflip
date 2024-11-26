baae25631f5d1886c72614a87ff598c78e70c8a7
Josh Bailey <josh@vandervecken.com>
2024-11-26 05:59:42 +0000

    more args

--- predict-cpu.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-set -o noglob
-exec docker run --rm -v /scratch:/scratch -ti anarkiwi/sidtableflip /sidtableflip/predict.py $*
--- predict.sh
+++ predict.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 set -o noglob
-exec docker run --gpus=all -eTORCHINDUCTOR_FX_GRAPH_CACHE=1 -eTORCHINDUCTOR_CACHE_DIR=/scratch/sidtableflip/inductorcache --rm -v /scratch:/scratch -ti anarkiwi/sidtableflip /sidtableflip/predict.py $*
+exec docker run --gpus=all -eTORCHINDUCTOR_FX_GRAPH_CACHE=1 -eTORCHINDUCTOR_CACHE_DIR=/scratch/sidtableflip/inductorcache --rm --name sidtableflip-predict -v /scratch:/scratch -ti anarkiwi/sidtableflip /sidtableflip/predict.py $*
